9002ccc353a8eaf8817d94219cf483a04493a4ed
jribeiroo <nicolas.jnr10@outlook.com>
2022-02-02 23:14:44 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/palma_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.palma_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+
+  FirebaseFirestore.instance
+      .collection('mensagens')
+      .doc()
+      .set({'texto': 'Ola', 'from': 'Jorge', 'read': false});
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
